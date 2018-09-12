140db62cb34bfedc1f3e15cd20553b1c845d6bde
andry <andriramananarivo@gmail.com>
2018-09-12 12:34:29 +0300

    user entity

---
+++ src/Api/CardBundle/Entity/user.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Api\CardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * user
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Api\CardBundle\Repository\userRepository")
+ */
+class user
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apiKey", type="string", length=50, unique=true)
+     */
+    private $apiKey;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return user
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set apiKey
+     *
+     * @param string $apiKey
+     *
+     * @return user
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    /**
+     * Get apiKey
+     *
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+}
+
