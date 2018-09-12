2afb09f664b72081d148b66501f6329c6f84537b
andry <andriramananarivo@gmail.com>
2018-09-12 13:58:41 +0300

    default controller

--- src/Api/CardBundle/Controller/DefaultController.php
+++ src/Api/CardBundle/Controller/DefaultController.php
@@ -3,15 +3,52 @@
 namespace Api\CardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('ApiCardBundle:Default:index.html.twig');
+class DefaultController extends Controller {
+
+    protected $_entity;
+
+    protected function getService() {
+    	return $this->loadService()->getRepo($this->_entity);
+    }
+
+    public function loadService() {
+        return $this->container->get('api_card.cardservice');
+    }
+
+    public final function rootAction() {
+    	return new JsonResponse(array(
+				'ressources' => array(
+					'api/card' => array(
+						'methods' => '[GET,HEAD]',
+						'desc' => 'return list of cards'
+					),
+					'api/card/{id}' => array(
+						'methods' => '[GET,HEAD]',
+						'args' => array(
+							'id' => 'int'
+						),
+						'desc' => 'return card by id'
+					),
+					'api/city/add' => array(
+						'methods' => '[POST]',
+						'args' => array(
+							'cardNumber' => 'string',
+							'pin' => 'int'
+						),
+						'desc' => 'add card'
+					),
+				),
+					'version' => '0.0.1'
+			));
+    }
+
+    protected function generateTokrn() {
+    	return $this->loadService()->generateAccessToken();
+    }
+
+    protected function findToken($entityName, $access_token) {
+    	return $this->loadService()->findToken($entityName, $access_token);
     }
 }
