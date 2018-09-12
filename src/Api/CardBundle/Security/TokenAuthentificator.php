3afe0e224c69aef8da14291f3e6adbd2072ece59
andry <andriramananarivo@gmail.com>
2018-09-12 12:43:55 +0300

    token auth

---
+++ src/Api/CardBundle/Security/TokenAuthentificator.php
@@ -0,0 +1,78 @@
+<?php
+namespace Api\VtcBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    
+    public function getCredentials(Request $request)
+    {
+        if (!$token = $request->headers->get('X-AUTH-TOKEN')) {
+
+            $token = null;
+        }
+
+        return array(
+            'token' => $token,
+        );
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiKey = $credentials['token'];
+
+        if (null === $apiKey) {
+            return;
+        }
+
+        return $userProvider->loadUserByUsername($apiKey);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+
+        if (!$token = $request->headers->get('X-AUTH-TOKEN')){
+            $data = array(
+                'error' => Response::HTTP_UNAUTHORIZED,
+                'message' => 'Authentication Required'
+            );
+
+            return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        }
+
+        $data = array(
+        	'error'	=> $exception->getCode(),
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        );
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
