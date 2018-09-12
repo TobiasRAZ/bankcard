91aaddcbdc01b3f87fd06e072a327582003ec4a7
andry <andriramananarivo@gmail.com>
2018-09-12 11:32:34 +0300

    setup sf2.8

---
+++ README.md
@@ -0,0 +1,4 @@
+bankcard
+========
+
+A Symfony project created on September 12, 2018, 10:20 am.
---
+++ src/Api/CardBundle/ApiCardBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Api\CardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiCardBundle extends Bundle
+{
+}
