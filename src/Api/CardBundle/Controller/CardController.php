d5cb6bf74c4af8d52257f64a7bbda0f2b156e8f6
andry <andriramananarivo@gmail.com>
2018-09-12 14:28:58 +0300

    routing

---
+++ src/Api/CardBundle/Controller/CardController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Api\CardBundle\Controller;
+
+use Api\VtcBundle\Entity\Card;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Api\VtcBundle\Controller\DefaultController;
+
+class CardController extends DefaultController {
+	protected $_entity = 'Card';
+
+	/**
+	 * List of cards
+	 */
+	public function indexAction() {
+		$cards = $this->getService()->getAll();
+		return new JsonResponse($cards);
+	}
+
+	/**
+	 * Get card by id
+	 */
+	public function getByIdAction($id) {
+		$city = $this->getService()->getById($id);
+		return new JsonResponse(city);
+	}
+
+	/**
+	 * Add new card
+	 */
+	public function addCardAction(Request $req) {
+		$card = new Card();
+		$card->setCardNumber($req->request->get('cardNumber'));
+		$card->setPin($req->request->get('pin'));
+		
+		$validator = $this->get('validator');
+		$errors = validator->validate($card);
+
+		$validationError = array();
+		if (count($errors) > 0) {
+			foreach ($errors->getIterator() as $key => $value) {
+				$err['message'] = $value->getMessage();
+				$err['parameter'] = $value->getPropertyPath();
+				array_push($validationError, $err);
+			}
+			return new JsonResponse($validationError);
+		}
+		$card = $this->getService()->add($card);
+		return new JsonResponse(array('success' => $card));
+	}
+}
---
+++ src/Api/CardBundle/EventListener/ExceptionListner.php
@@ -0,0 +1,44 @@
+<?php
+namespace Api\VtcBundle\EventListener;
+ 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+ 
+/**
+* 
+*/
+class ExceptionListener
+{
+	public function onKernelException(GetResponseForExceptionEvent $event)
+	{
+		$response = new Response();
+		$exception = $event->getException();
+        $response->headers->set('Content-Type', 'application/json');
+
+	    if ($event->getException() instanceof NotFoundHttpException) {
+
+	    	$error = array(
+	    			'error' => $exception->getStatusCode(),
+	    			'message' => 'The ressource you tried to load was not found on this server'
+	    		);
+        	$response->setStatusCode($exception->getStatusCode());
+			$response->setContent( json_encode($error) );
+		    $event->setResponse($response);
+
+	    }
+
+	    if ($event->getException() instanceof MethodNotAllowedHttpException) {
+
+	    	$error = array(
+	    			'error' => $exception->getStatusCode(),
+	    			'message' => 'Method not allowed for this ressource'
+	    		);
+        	$response->setStatusCode($exception->getStatusCode());
+			$response->setContent( json_encode($error) );
+		    $event->setResponse($response);
+	    }
+	    	
+	}
+}
