13c225f546bed9ce72b398f80c7fd66cad34ee28
andry <andriramananarivo@gmail.com>
2018-11-13 11:01:04 +0300

    generate account controller

---
+++ src/Api/CardBundle/Controller/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Api\CardBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Api\CardBundle\Controller\DefaultController;
+
+
+class AccountController extends DefaultController
+{
+
+	public function addAction(Request req)
+	{
+		# code...
+	}
+}
