5d69e22616985c58a2e53bacc2b89b576dc41fd1
Josias <josiasrazafimamonjy@gmail.com>
2018-12-13 10:02:34 +0300

    create new bundle sync

---
+++ src/Fintek/SyncBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fintek\SyncBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FintekSyncBundle:Default:index.html.twig');
+    }
+}
