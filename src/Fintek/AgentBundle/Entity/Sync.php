d638500ca6a76af69d3019bd9c0272fb59e396cd
Josias <josiasrazafimamonjy@gmail.com>
2018-12-13 10:05:35 +0300

    create entity sync

---
+++ src/Fintek/AgentBundle/Entity/Sync.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Fintek\AgentBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sync
+ *
+ * @ORM\Table(name="sync")
+ * @ORM\Entity(repositoryClass="Fintek\AgentBundle\Repository\SyncRepository")
+ */
+class Sync
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Sync
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
+
